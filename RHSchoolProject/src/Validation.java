f24bfc0ae26abe4ceca02c83f799cba18eec4d1d
ruqaia-hamad <ruq77771@gmail.com>
2022-10-31 12:07:23 +0400

    add validation class for login & password

---
+++ RHSchoolProject/src/Validation.java
@@ -0,0 +1,21 @@
+
+public class Validation {
+	
+	
+	public  void validateUsername(String username) throws IllegalArgumentException {
+		String teacherName = "Ruqaia";
+       
+        if (!username.matches(teacherName)) {
+            throw new IllegalArgumentException("Invalid username.");
+        }
+    }
+	public void validatePassword(String password) throws IllegalArgumentException {
+		String userPassword ="1234";
+		
+		if(!password.matches(userPassword)) {
+			  throw new IllegalArgumentException("Invalid password");
+		}
+       
+	}
+
+}
